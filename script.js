8b72387501a9d050d79e2a1b2992888e96e2f866
BricoBC <barajas.c.bruno.n@gmail.com>
2022-12-10 23:10:35 -0600

    Responsive design: Nav derecho del correo y boton hamburguesa

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const btnHamburger = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
-
+btnHamburger.addEventListener('click', toggleMobilepMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobilepMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
 
